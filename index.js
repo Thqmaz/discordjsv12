8a789fbee90135aaf53857f396cb3171a184e741
Thqmaz <t.groenendijk.tg@gmail.com>
2020-10-14 15:23:28 +0200

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client({ disableEveryone: false });
+const config = require("./config.json");
+
+bot.on("ready", async() => {
+    console.log(`${bot.user.username} started up successfully`)
+});
+
+bot.on("message", async message => {
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command == "ping") {
+        message.reply("Pong!")
+    }
+});
